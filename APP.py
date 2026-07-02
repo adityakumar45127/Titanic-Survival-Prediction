43a5c6f5b3c39d3cf64a878e4579ec2748c9770b
Aditya Kumar <adityakumar45127@gmail.com>
2026-07-02 22:41:10 +0530

    Initial commit

---
+++ APP.py
@@ -0,0 +1,50 @@
+
+import sys
+import sklearn
+
+print(sys.executable)
+print(sklearn.__version__)
+
+
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load trained pipeline
+import os
+
+print(os.getcwd())
+
+print(os.path.abspath("complete_pipeline.pkl"))
+
+model = joblib.load("complete_pipeline.pkl")
+
+
+st.title("🚢 Titanic Survival Prediction App")
+
+st.write("Enter passenger details to predict survival:")
+
+# User Inputs
+pclass = st.selectbox("Passenger Class", [1, 2, 3])
+sex = st.selectbox("Sex", ["male", "female"])
+age = st.slider("Age", 0, 80, 25)
+fare = st.number_input("Fare", min_value=0.0, value=30.0)
+embarked = st.selectbox("Embarked", ["C", "Q", "S"])
+
+# Convert input into DataFrame
+input_data = pd.DataFrame({
+    "Pclass": [pclass],
+    "Sex": [sex],
+    "Age": [age],
+    "Fare": [fare],
+    "Embarked": [embarked]
+})
+
+# Prediction
+if st.button("Predict"):
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.success("✅ The passenger is likely to SURVIVE")
+    else:
+        st.error("❌ The passenger is NOT likely to survive")
